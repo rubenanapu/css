92edaef1e2a6cfca0cc09543db50b7ba66d3fbd3
Ruben Alves <rubenanapu@hotmail.com>
2022-03-13 17:15:01 -0300

    Opening Mobile Navigation Bar when clicking the "hamburger" button, and closing when backdrop is clicked

--- basics-01-understanding-the-course-project-setup/shared.js
+++ basics-01-understanding-the-course-project-setup/shared.js
@@ -2,6 +2,8 @@ var backdrop = document.querySelector(".backdrop");
 var modal = document.querySelector(".modal");
 var buttons = document.querySelectorAll(".plan button");
 var closeModalButton = document.querySelector(".modal__action--negative");
+var hamburgerButton = document.querySelector(".toggle-button");
+var mobileSideNavBar = document.querySelector(".mobile-nav");
 
 for (var i = 0; i < buttons.length; i++) {
   buttons[i].addEventListener("click", function () {
@@ -15,5 +17,14 @@ function closeModal(){
     modal.style.display = "none";
 }
 
+function showSideNavigationBar(){
+  mobileSideNavBar.style.display = "block";
+  backdrop.style.display = 'block';
+}
+
 closeModalButton.addEventListener('click', closeModal);
-backdrop.addEventListener('click', closeModal);
+backdrop.addEventListener('click', function(){
+  mobileSideNavBar.style.display = "none";;
+  closeModal();
+});
+hamburgerButton.addEventListener('click', showSideNavigationBar);
