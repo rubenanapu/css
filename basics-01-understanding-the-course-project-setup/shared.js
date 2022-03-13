98c9af6a5e51e660c3da423dc22dbfc0b8d278d6
Ruben Alves <rubenanapu@hotmail.com>
2022-03-13 17:21:53 -0300

    Using open css class to easily show and hide modal and Mobile Navigation Bar

--- basics-01-understanding-the-course-project-setup/shared.js
+++ basics-01-understanding-the-course-project-setup/shared.js
@@ -7,24 +7,33 @@ var mobileSideNavBar = document.querySelector(".mobile-nav");
 
 for (var i = 0; i < buttons.length; i++) {
   buttons[i].addEventListener("click", function () {
-    backdrop.style.display = "block";
-    modal.style.display = "block";
+    // backdrop.style.display = "block";
+    // modal.style.display = "block";
+    backdrop.classList.add("open");
+    modal.classList.add("open");
   });
 }
 
 function closeModal(){
-    backdrop.style.display = "none";
-    modal.style.display = "none";
+    // backdrop.style.display = "none";
+    // modal.style.display = "none";
+
+    backdrop.classList.remove('open');
+    modal.classList.remove('open');
 }
 
 function showSideNavigationBar(){
-  mobileSideNavBar.style.display = "block";
-  backdrop.style.display = 'block';
+  // mobileSideNavBar.style.display = "block";
+  // backdrop.style.display = 'block';
+
+  mobileSideNavBar.classList.add("open");
+  backdrop.classList.add("open");
 }
 
 closeModalButton.addEventListener('click', closeModal);
 backdrop.addEventListener('click', function(){
-  mobileSideNavBar.style.display = "none";;
+  // mobileSideNavBar.style.display = "none";
+  mobileSideNavBar.classList.remove("open");
   closeModal();
 });
 hamburgerButton.addEventListener('click', showSideNavigationBar);
